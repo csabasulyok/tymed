31a6162a208f0b6818c4bd8ef4709f33cdd4a49d
Csaba Sulyok <csaba.sulyok@gmail.com>
2017-04-19 18:29:49 +0300

    release version 0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name='tymed',
   packages=['tymed'],
-  version='0.0.1',
+  version='0.0.2',
   description='Easily measure execution time per Python method.',
   author='Csaba Sulyok',
   author_email='csaba.sulyok@gmail.com',

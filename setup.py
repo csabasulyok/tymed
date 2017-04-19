39124c1de36846c7fa2b25c9df66c992ab98e486
Csaba Sulyok <csaba.sulyok@gmail.com>
2017-04-19 15:26:25 +0300

    attempt travis ci setup

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(
   name='tymed',
-  packages=['tymed'],  # this must be the same as the name above
+  packages=['tymed'],
   version='0.0.1',
   description='Easily measure execution time per Python method.',
   author='Csaba Sulyok',
   author_email='csaba.sulyok@gmail.com',
   url='https://github.com/csabasulyok/tymed',
-  download_url='https://github.com/csabasulyok/tymed/archive/0.0.1.tar.gz',  # I'll explain this in a second
+  download_url='https://github.com/csabasulyok/tymed/archive/0.0.1.tar.gz',
   keywords=['timer'],
   classifiers=[],
 )
